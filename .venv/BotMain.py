353ab7b9a9385d0f9b3e3515e7c9e9f64115f415
SpencerLeBard <slebard@gmail.com>
2020-11-24 12:51:57 -0700

    xpath

--- .venv/BotMain.py
+++ .venv/BotMain.py
@@ -5,14 +5,7 @@ import time
 # Set breakpoint() in Python to call pudb
 # export PYTHONBREAKPOINT = "pudb.set_trace"
 
-# import pagerduty
-
-
 # driver = webdriver.Firefox()
-
-# happy case - item is available
-# driver.get("https://www.bestbuy.com/site/nvidia-titan-rtx-24gb-gddr6-pci-express-3-0-graphics-card/6320585.p?skuId=6320585")
-
 
 driver = webdriver.Firefox(
     executable_path=r'C:\Users\sleba\AppData\Local\Programs\geckodriver-v0.28.0-win64\geckodriver.exe')
@@ -20,33 +13,40 @@ driver.get(
     'https://www.bestbuy.com/site/dynex-6-hdmi-cable-black/6405508.p?skuId=6405508')
 
 
-class PagePoller:
-    def __init__(self, url):
-        self.url = url
-        self.createBrowser()
-
-    def createBrowser(self):
-        self.driver = webdriver.Firefox()
-        self.driver.get(self.url)
-
-    def refreshPage(self):
-        self.driver.close()
-        self.driver.quit()
-        self.createBrowser()
-
-    def checkAvailable(self):
-        self.driver.find_element_by_xpath(
-            "//button[@data-sku-id='6405508'].click()")
-
-
 def checkAvailable(self):
-    addToCartButton = self.driver.find_element_by_class_name(
-        "add-to-cart-button")
-    if ("btn-disabled" in addToCartButton.get_attribute("class")):
-        return False
-    else:
-        addToCartButton.click()
-        return True
+    button = self.driver.find_element_by_xpath(
+        "/html[1]/body[1]/div[3]/main[1]/div[2]/div[3]/div[2]/div[1]/div[1]/div[7]/div[1]/div[1]/div[1]/div[1]/button[1]")
+    button.click()
+    checkAvailable(self)
+
+
+# class PagePoller:
+#     def __init__(self, url):
+#         self.url = url
+#         self.createBrowser()
+
+#     def createBrowser(self):
+#         self.driver = webdriver.Firefox()
+#         self.driver.get(self.url)
+
+#     def refreshPage(self):
+#         self.driver.close()
+#         self.driver.quit()
+#         self.createBrowser()
+
+#     def checkAvailable(self):
+#         self.driver.find_element_by_xpath(
+#             "//button[@data-sku-id='6405508'].click()")
+
+
+# def checkAvailable(self):
+#     addToCartButton = self.driver.find_element_by_class_name(
+#         "add-to-cart-button")
+#     if ("btn-disabled" in addToCartButton.get_attribute("class")):
+#         return False
+#     else:
+#         addToCartButton.click()
+#         return True
 
 
 # good
