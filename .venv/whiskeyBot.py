cbd5989bc0622f4f959fe2fea61e17a38f387106
SpencerLeBard <slebard@gmail.com>
2020-12-01 18:55:48 -0700

    almost printing results

--- .venv/whiskeyBot.py
+++ .venv/whiskeyBot.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 import time
 
+
+# NOTE BLANTONS
+
 driver = webdriver.Firefox(
     executable_path=r'C:\Users\sleba\AppData\Local\Programs\geckodriver-v0.28.0-win64\geckodriver.exe')
 driver.get(
@@ -46,3 +49,41 @@ time.sleep(2)
 zipcode_input_button = driver.find_element_by_xpath(
     "/html[1]/body[1]/div[2]/div[1]/div[2]/main[1]/div[3]/form[1]/input[4]")
 zipcode_input_button.click()
+
+time.sleep(2)
+
+location_results = driver.find_elements_by_id("results")
+location_search_string = location_results.find("Hailey")
+if(location_search_string in location_results):
+    print("yes")
+else:
+    print(location_results)
+    print("no")
+
+# all_results = driver.find_elements_by_xpath("//*[contains(text(),'Hailey')]")
+# print len(all_results)
+
+# NOTE create new tab instead of new window, export to notepad?
+
+# NOTE EAGLE RARE
+
+# driver = webdriver.Firefox(
+#     executable_path=r'C:\Users\sleba\AppData\Local\Programs\geckodriver-v0.28.0-win64\geckodriver.exe')
+# driver.get(
+#     'https://mixblendenjoy.com/product/?nabca=16850&query=16850')
+
+# age_verification_button = driver.find_element_by_xpath(
+#     "/html[1]/body[1]/div[1]/form[1]/input[2]")
+# age_verification_button.click()
+
+# time.sleep(2)
+
+# limited_availability_button = driver.find_element_by_xpath(
+#     "/html[1]/body[1]/div[2]/div[1]/div[3]/div[2]/div[2]/a[1]/img[1]")
+# limited_availability_button.click()
+
+# eagle_rare_button = driver.find_element_by_xpath(
+#     "/html[1]/body[1]/div[2]/div[1]/div[1]/main[1]/article[1]/div[1]/ul[1]/li[14]/a[1]")
+# eagle_rare_button.click()
+
+# time.sleep(2)
