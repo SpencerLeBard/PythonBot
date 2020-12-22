527fc8e821ccc26f1cbdf784567cd82a265a2fcc
SpencerLeBard <slebard@gmail.com>
2020-12-22 14:27:28 -0700

    plotting

--- standardplot.py
+++ standardplot.py
@@ -1,7 +1,16 @@
-import matplotlib.pyplot as plt
-import numpy as np
+# import matplotlib.pyplot as plt
+# import numpy as np
 
-# Create a list of evenly-spaced numbers over the range
-x = np.linspace(0, 20, 100)
-plt.plot(x, np.sin(x))       # Plot the sine of each x point
-plt.show()                   # Display the plot
+# # Create a list of evenly-spaced numbers over the range
+# x = np.linspace(0, 20, 100)
+# plt.plot(x, np.sin(x))       # Plot the sine of each x point
+# plt.show()                   # Display the plot
+
+
+# import matplotlib.pyplot as plt
+
+# plt.plot([1, 2, 3, 4], [5, 10, 15, 20])
+# plt.title("Linear relationship", fontsize=20)
+# plt.xlabel("X Axis", fontsize=20)
+# plt.ylabel("Y Axis", fontsize=20)
+# plt.show()
