7406e108ecad18b5cf7a5e6216c759e4a4a88d54
SpencerLeBard <slebard@gmail.com>
2020-12-28 08:19:21 -0700

    importing

--- standardplot.py
+++ standardplot.py
@@ -1,10 +1,10 @@
-# import matplotlib.pyplot as plt
-# import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 
-# # Create a list of evenly-spaced numbers over the range
-# x = np.linspace(0, 20, 100)
-# plt.plot(x, np.sin(x))       # Plot the sine of each x point
-# plt.show()                   # Display the plot
+# Create a list of evenly-spaced numbers over the range
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))       # Plot the sine of each x point
+plt.show()                   # Display the plot
 
 
 # import matplotlib.pyplot as plt
@@ -14,3 +14,14 @@
 # plt.xlabel("X Axis", fontsize=20)
 # plt.ylabel("Y Axis", fontsize=20)
 # plt.show()
+
+# import pandas as pd
+
+# data = {
+#     'cars': [5, 4, 1, 7],
+#     'boats': [2, 6, 0, 2]
+# }
+
+# vehicals = pd.DataFrame(data, index=['Peter', 'Sarah', 'Ali', 'John'])
+
+# print(vehicals.info())
